0697c7bd6decdd44a4724b649b37c74d3920f662
JDEA-NLP <dingliang1@jd.com>
2022-08-06 19:23:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# RLFW-NAT
+
+This repo contains the codes for our [ACL paper](https://aclanthology.org/2022.acl-long.172.pdf)
+
+> Redistributing Low-Frequency Words: Making the Most of Monolingual Data in Non-Autoregressive Translation
+> Liang Ding, Longyue Wang, Shuming Shi, Dacheng Tao, Zhaopeng Tu
+
+
+### Download model 
+The pretrained English-Romanian model and their corresponding dict file can be downloaded from:  
+
+Dataset | Dict | Model
+---|---|---  
+[WMT16 English-Romanian] | [dict (.zip)](https://drive.google.com/uc?id=1o1h5ZsTJOn4Tyb9l3aigqY7bsVXLtwC5) | [model (.zip)](https://drive.google.com/uc?id=1u-dFW6aQXHPs67gaD1GAtiIlo41aK3Py)  
+
+# License
+The license is CC-BY-NC 4.0.
+The license applies to the pre-trained models as well.
+
+# Citation
+
+Please cite as:
+
+```bibtex
+@inproceedings{ding2022redistributing,
+    title = {Redistributing Low-Frequency Words: Making the Most of Monolingual Data in Non-Autoregressive Translation},
+    author = {Liang Ding, Longyue Wang, Xuebo Liu, Derek F. Wong, Dacheng Tao and Zhaopeng Tu}
+    booktitle = {ACL},
+    year = {2022}
+}
+```
---
+++ train_lev.sh
@@ -0,0 +1,62 @@
+#! /usr/bin/bash
+export MKL_SERVICE_FORCE_INTEL=1;
+export MKL_THREADING_LAYER=GNU;
+
+ps aux|grep /root/miniconda2/envs/py3.7/bin/python|awk '{print $2}'|xargs kill -9
+
+pip install -e ./fairseq_lev/
+
+set -e
+
+s=$SRC
+t=$TGT
+
+databin=${databin}
+task=lev_${databin}
+
+if [ ! -d ./checkpoint/${s}${t}/${task} ]; then
+  mkdir -p ./checkpoint/${s}${t}/${task}
+fi
+
+cp $0 model/${task}
+
+echo ">> training ${task}"
+
+nohup fairseq-train ${databin} \
+  --save-dir ./checkpoint/${s}${t}/${task} \
+  --ddp-backend=no_c10d --fp16 \
+  --task translation_lev \
+  --criterion nat_loss \
+  --arch levenshtein_transformer \
+  --label-smoothing 0.1 \
+  --attention-dropout 0.0 \
+  --activation-dropout 0.0 \
+  --dropout 0.2 \
+  --noise random_delete \
+  --share-decoder-input-output-embed \
+  --optimizer adam --adam-betas '(0.9,0.98)' \
+  --lr 1e-07 --max-lr 1e-3 --lr-scheduler cosine \
+  --warmup-init-lr 1e-07 --warmup-updates 10000 --lr-shrink 1 --lr-period-updates 60000 \
+  --max-update 70000 \
+  --weight-decay 0.0 --clip-norm 0.1 \
+  --max-tokens 20000 --update-freq 3 \
+  --decoder-learned-pos \
+  --encoder-learned-pos \
+  --apply-bert-init \
+  --no-progress-bar --log-format 'simple' --log-interval 100 \
+  --fixed-validation-seed 7 \
+  --seed 1 \
+  --save-interval-updates 2000 \
+  --keep-last-epochs 0 \
+  --fp16-scale-tolerance 0.1 >./checkpoint/${s}${t}/${task}/train.log 2>&1 &
+wait
+
+
+# for small dataset e.g. en-ro, we just change following settings:
+#   --attention-dropout 0.3 \
+#   --activation-dropout 0.3 \
+#   --dropout 0.3 \
+#   --share-all-embeddings \
+#   --warmup-updates 4000--lr-period-updates 21000 \
+#   --max-update 25000 \
+#   --weight-decay 0.0001
